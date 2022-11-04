56d7f27872c107848872f8ced5be590edd6a3013
Aditya Bhattad <adityabhattad18@gmail.com>
2022-11-04 13:36:20 +0530

    Initial Commit

---
+++ hardhat.config.js
@@ -0,0 +1,48 @@
+require("@nomicfoundation/hardhat-toolbox");
+require("hardhat-deploy");
+require("dotenv").config();
+
+const POLYGON_RPC_URL = process.env.POLYGON_RPC_URL;
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
+const POLYSCAN_API_KEY = process.env.POLYSCAN_API_KEY;
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+	solidity: "0.8.17",
+	networks: {
+		hardhat: {
+			chainId: 1337,
+			blockConfirmations: 1,
+		},
+		localhost: {
+			chainId: 1337,
+			blockConfirmations: 1,
+		},
+		mumbai: {
+			chainId: 80001,
+			blockConfirmations: 6,
+			url: POLYGON_RPC_URL,
+			accounts:[PRIVATE_KEY]
+		},
+	},
+	namedAccounts: {
+		deployer: {
+			default:0
+		},
+		notDeployer: {
+			default:1
+		},
+		secondNonDeployer: {
+			default:2
+		},
+		thirdNonDeployer: {
+			default:3
+		}
+	},
+	etherscan: {
+		apiKey:POLYSCAN_API_KEY
+	},
+	mocha: {
+		timeout:500000 // 500 seconds.
+	}
+};
